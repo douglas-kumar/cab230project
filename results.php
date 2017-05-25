f15f894005f0db8c4e6435753cd6ed688e56d8e4
Douglas <douglaskumar11@gmail.com>
2017-05-25 12:14:04 +1000

    Search result processing complete

--- results.php
+++ results.php
@@ -1,8 +1,37 @@
 ﻿<?php require './includes/partials/header.inc' ?>
 <?php require './includes/scripts/results.inc' ?>
 <?php
-	if (!isset($_POST['submit'])) {
+	if (empty($results)) {
 		$results = 0;
+	} else {
+
+		global $database;
+		global $errors;
+
+		$rats = array("itemId" => "rating");
+		//print_r($rats);
+		//echo "<br/>";
+
+		try {
+			foreach ($results as $result) {
+				$query = "SELECT AVG(rating) AS rat FROM reviews GROUP BY itemID Having itemId = :id";
+				$stmt = $database->prepare($query);
+				$stmt->bindParam("id", $result['id']);
+				$stmt->execute();
+
+				if ($stmt->rowCount() > 0) {
+					$avg = $stmt->fetch();
+					$rats[$result['id']] = $avg['rat'];
+				} else {
+					$rats[$result['id']] = "No ratings";
+				}
+				// print_r($rats);
+				// echo "<br />";
+				// print_r($results);
+			}
+		} catch (PDOException $e) {
+			die($e.getMessage());
+		}
 	}
 ?>
 
@@ -21,20 +50,32 @@
 	<table>
 		<tr>
 			<th>Rating</th>
+			<th>Code</th>
 			<th>Name</th>
+			<th>Street</th>
+			<th>Suburb</th>
 		</tr>
 
 		<?php
 			if ($results != 0) {
 				foreach($results as $result) {
 					echo "<tr><td>";
-					for ($i = 0; $i < 3; $i++) {
-						echo "<img src=\"public/images/star.png\">";
+					if ($rats[$result['id']] != "No ratings") {
+						for ($i = 0; $i < floor($rats[$result['id']]); $i++) {
+							echo "<img src=\"public/images/star.png\">";
+						}
+					} else {
+						echo"<i>No ratings</i>";
 					}
-					echo "</td><td><a href=\"park.php?itemId={$result['id']}\">{$result['name']}</a></td>";
+					echo "</td>
+						  <td>{$result['parkCode']}</td>
+						  <td><a href=\"park.php?itemId={$result['id']}\">{$result['name']}</a></td>
+						  <td>{$result['street']}</td>
+						  <td>{$result['suburb']}</td>
+						  </tr>";
 				}
 			} else {
-				echo "<tr><td>No items matched your search.</tr></td>";
+				echo "<tr><td colspan=\"5\">No items matched your search.</td></tr>";
 			}
 		?>
 
